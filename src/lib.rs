f9bf1b51c4b476049fa216d7914dbfb20309e605
Mark Zuschlag <mzuschlag@MBP-0518.local>
2019-01-25 15:53:46 -0800

    add tests for Config creation and run function

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(Debug)]
 pub struct Config {
   pub query: String,
   pub filename: String,
@@ -21,5 +22,55 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+impl PartialEq for Config {
+    fn eq(&self, other: &Config) -> bool {
+        self.query == other.query && self.filename == other.filename
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn create_new_config() {
+        let args = valid_args();
+        assert_eq!(Config::new(&args).unwrap(), Config { query: "two".to_string(), filename: "poem.txt".to_string() })
+    }
+    #[test]
+    fn create_invalid_config() {
+        let args = invalid_args_len();
+        assert!(Config::new(&args).is_err(), "not enough arguments")
+    }
+
+    #[test]
+    fn valid_run() {
+        let args = valid_args();
+        let config = Config::new(&args).unwrap();
+        if let Err(e) = run(config) {
+            panic!("Application error: {}", e);
+        }
+        assert!(true)
+    }
+
+    #[test]
+    fn invalid_run() {
+        let args = invalid_args_filename();
+        let config = Config::new(&args).unwrap();
+        assert!(run(config).is_err())
+    }
+
+    fn valid_args() -> Vec<String> {
+        vec!("one".to_string(), "two".to_string(), "poem.txt".to_string())
+    }
+
+    fn invalid_args_len() -> Vec<String> {
+        vec!("one".to_string(), "two".to_string())
+    }
+
+    fn invalid_args_filename() -> Vec<String> {
+        vec!("one".to_string(), "two".to_string(), "fake.txt".to_string())
     }
 }
